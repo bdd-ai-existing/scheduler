de93b09a14547eec802b420d13e3852aa2817f61
akbar <jamalakbaralam@gmail.com>
2024-12-24 13:32:11 +0700

    update mongodb config

--- db/base_mongodb.py
+++ db/base_mongodb.py
@@ -1,8 +1,8 @@
 from config import settings
 from pymongo import MongoClient
 
-MONGO_URI = f"mongodb://{settings.MONGODB_USER}:{settings.MONGODB_PASS}@{settings.MONGODB_HOST}:{settings.MONGODB_PORT}/?authMechanism=SCRAM-SHA-256&authSource={settings.MONGODB_NAME}"
-# MONGO_URI = f"mongodb://{settings.MONGODB_USER}:{settings.MONGODB_PASS}@{settings.MONGODB_HOST}:{settings.MONGODB_PORT}/{settings.MONGODB_NAME}?authSource=admin&retryWrites=true&w=majority"
+MONGO_URI = f"mongodb://{settings.MONGODB_USER}:{settings.MONGODB_PASS}@{settings.MONGODB_HOST_PRIMARY}:{settings.MONGODB_PORT},{settings.MONGODB_HOST_SECONDARY}:{settings.MONGODB_PORT}/?authMechanism=SCRAM-SHA-256&readPreference=secondaryPreferred&authSource={settings.MONGODB_NAME}"
+# MONGO_URI = f"mongodb://{settings.MONGODB_USER}:{settings.MONGODB_PASS}@{settings.MONGODB_HOST_PRIMARY}:{settings.MONGODB_PORT}/{settings.MONGODB_NAME}?authSource=admin&retryWrites=true&w=majority"
 
 def get_database():
     client = MongoClient(MONGO_URI)
